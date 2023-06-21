c327501defe4fdcc49539be2c1c163c56109750d
Nicholas Nielsen <nicknielsen5@gmail.com>
2023-06-21 22:46:10 +0000

    pseudosecured api key

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
+import sqlite3 as sql
 import openai
-
 #main loop for progressive conversation
 def main():
-    # Load your API key from an environment variable or secret management service
-    openai.api_key = "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"
-    #prompt for chatgpt to follow in its responses1
-    SystemPrompt = "you are a helpful assistant"
+    tempsecure = "sk-9cvUng0soHvTdaNSddDCCyT3BlbkFJvlrjARpF48JRKUXMmJsk-4vcUn0gsoHvTdaNSDcYcT3BlbkFJvlMrjARpF49C60VUXMmJsk-25FUn5gosHvTNaDSCdYcT3BlKgjJvlMrjARpF49C60VUXMmD"
+    #DONT REVEAL THIS KEY! ENCRYPT AND HIDE IT LATER
+    openai.api_key = tempsecure[51:102]
+    #DONT REVEAL THIS KEY ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    #prompt for chatgpt to follow in its responses
+    SystemPrompt = "you are wildly pessimistic and dont like to answer questions"
     #holds line of conversation between you and chatgpt
     chathistory = [{"role": "system", "content": SystemPrompt}]
 
@@ -27,5 +30,5 @@ def main():
         chathistory.append(resmessage)
         print("Response: "+chat_completion.choices[0].message.content)
 
-
+print("-dumpmem || clears conversation topic.\n -histlog || show the logged conversation")
 main()
